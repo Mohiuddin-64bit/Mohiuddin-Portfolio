6b1fc49934c1b07619afdbb7432296b78e69bc08
Mohiuddin <mohiuddin.niddu@gmail.com>
2024-06-03 15:24:30 +0600

    refactor: Update baseAPI configuration to use new backend URL

--- src/redux/api/baseAPI.js
+++ src/redux/api/baseAPI.js
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
-    // baseUrl: "https://community-eats-backend.vercel.app",
+    baseUrl: "https://portfolio-backend-mu-lake.vercel.app",
   }),
   endpoints: () => ({}),
 });
